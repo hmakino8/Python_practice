90a099697641e2e16c725c16099541b519da2296
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-11-03 14:45:51 +0900

    🥅 404エラー発生
    404エラーでlocalhostにアクセスできない。

--- django/myvenv/myproject/crud/models.py
+++ django/myvenv/myproject/crud/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)  # 商品名を文字列で定義　最大文字列長は200
+    price = models.PositiveIntegerField()  # 価格を整数で定義　正の整数のみ
+
+    def __str__(self):
+        return self.name
--- django/myvenv/myproject/crud/views.py
+++ django/myvenv/myproject/crud/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Product  # .modelsとすることで、同じディレクトリにあるmodels.pyを参照
 
 
 class TopView(TemplateView):
     template_name = "top.html"  # テンプレートファイルの指定
+
+
+"""
+ListViewは、template_nameを指定しない場合、
+モデル名_list.htmlというテンプレートファイルを参照する
+"""
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "list.html"
+    paginate_by = 3
