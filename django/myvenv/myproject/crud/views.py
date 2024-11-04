3645ff16e0c07ca5b22f3316c4dac0160d29567d
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-11-05 00:54:29 +0900

    ✨ ログイン機能の実装

--- django/myvenv/myproject/crud/views.py
+++ django/myvenv/myproject/crud/views.py
@@ -3,6 +3,9 @@ from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Product  # .modelsとすることで、同じディレクトリにあるmodels.pyを参照
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class TopView(TemplateView):
@@ -15,13 +18,13 @@ ListViewは、template_nameを指定しない場合、
 """
 
 
-class ProductListView(ListView):
+class ProductListView(LoginRequiredMixin, ListView):
     model = Product
     template_name = "list.html"
     paginate_by = 3
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(LoginRequiredMixin, CreateView):
     model = Product
     fields = '__all__'
 
@@ -32,12 +35,21 @@ UpdateViewは、指定されたプライマリキーに基づいて
 '''
 
 
-class ProductUpdateView(UpdateView):
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
     model = Product
     fields = '__all__'
     template_name_suffix = '_update_form'
 
 
-class ProductDeleteView(DeleteView):
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = Product
     success_url = reverse_lazy('list')
+
+
+class LoginView(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
+
+class LogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'top.html'
