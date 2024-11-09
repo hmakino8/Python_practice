408725918935b2192399a63393cd4b8c72b8ebbe
Hiroaki Makino <8makino.42tokyo@gmail.com>
2024-11-10 00:32:52 +0900

    feat: Implement HomeView

--- meeting_scheduler/myproject/core/views.py
+++ meeting_scheduler/myproject/core/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from .models import *
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
+    model = Room
     template_name = 'core/home.html'
 
 
-class RoomView(TemplateView):
+class RoomView(DetailView):
+    model = Room
     template_name = 'core/room.html'
